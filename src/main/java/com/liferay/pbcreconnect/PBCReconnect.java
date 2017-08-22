b287c31adf017294c602bffe5023c16477fea557
Gregory Amerson <gregory.amerson@liferay.com>
2017-08-22 14:42:23 -0500

    SF

--- src/main/java/com/liferay/pbcreconnect/PBCReconnect.java
+++ src/main/java/com/liferay/pbcreconnect/PBCReconnect.java
@@ -33,7 +33,66 @@ import java.util.concurrent.TimeUnit;
  */
 public class PBCReconnect {
 
-	static class Checker extends AbstractScheduledService {
+	public static void main(String[] args) {
+		Checker checker = new Checker("google.com");
+
+		checker.startAsync();
+
+		try {
+			System.in.read();
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void post() throws Exception {
+		final WebRequest requestSettings = new WebRequest(
+			new java.net.URL("https://1.1.1.1/login.html"), HttpMethod.POST);
+
+		requestSettings.setAdditionalHeader(
+			"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		requestSettings.setAdditionalHeader("Content-Type", "application/x-www-form-urlencoded");
+		requestSettings.setAdditionalHeader("Referer", "https://1.1.1.1/login.html");
+		requestSettings.setAdditionalHeader("Accept-Language", "en-US");
+		requestSettings.setAdditionalHeader("Accept-Encoding", "gzip,deflate");
+		requestSettings.setAdditionalHeader("User-Agent", "Java");
+		requestSettings.setAdditionalHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+		requestSettings.setAdditionalHeader("X-Requested-With", "XMLHttpRequest");
+		requestSettings.setAdditionalHeader("Cache-Control", "no-cache");
+		requestSettings.setAdditionalHeader("Pragma", "no-cache");
+		requestSettings.setAdditionalHeader("Host", "1.1.1.1");
+		requestSettings.setAdditionalHeader("Connection", "keep-alive");
+		requestSettings.setAdditionalHeader("Upgrade-Insecure-Requests", "1");
+		requestSettings.setAdditionalHeader("Cookie", "GUEST_LANGUAGE_ID=en_US; COOKIE_SUPPORT=true");
+		requestSettings.setAdditionalHeader("Origin", "http://1.1.1.1/login.html");
+
+		requestSettings.setRequestBody(
+			"buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network");
+
+		final WebClient webClient = new WebClient();
+
+		webClient.getOptions().setUseInsecureSSL(true);
+		webClient.getPage(requestSettings);
+		webClient.close();
+	}
+
+	public static boolean testInet(String site) {
+		try {
+			java.net.URL url = new java.net.URL("https://www.google.com");
+
+			URLConnection connection = url.openConnection();
+
+			connection.connect();
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static class Checker extends AbstractScheduledService {
 
 		public Checker(String site) {
 			_site = site;
@@ -62,73 +121,7 @@ public class PBCReconnect {
 		}
 
 		private final String _site;
-	}
 
-	public static void main(String[] args) {
-		Checker checker = new Checker("google.com");
-
-		checker.startAsync();
-
-		try {
-			System.in.read();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void post() throws Exception {
-		final WebClient webClient = new WebClient();
-
-		WebRequest requestSettings = new WebRequest(
-		new java.net.URL("https://1.1.1.1/login.html"), HttpMethod.POST);
-
-		requestSettings.setAdditionalHeader(
-		"Accept",
-		"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		requestSettings.setAdditionalHeader(
-		"Content-Type", "application/x-www-form-urlencoded");
-		requestSettings.setAdditionalHeader(
-		"Referer", "http://1.1.1.1/login.html");
-		requestSettings.setAdditionalHeader("Accept-Language", "en-US");
-		requestSettings.setAdditionalHeader("Accept-Encoding", "gzip,deflate");
-		requestSettings.setAdditionalHeader(
-		"User-Agent",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8");
-		requestSettings.setAdditionalHeader(
-		"Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
-		requestSettings.setAdditionalHeader(
-		"X-Requested-With", "XMLHttpRequest");
-		requestSettings.setAdditionalHeader("Cache-Control", "no-cache");
-		requestSettings.setAdditionalHeader("Pragma", "no-cache");
-		requestSettings.setAdditionalHeader("Host", "1.1.1.1");
-		requestSettings.setAdditionalHeader("Connection", "keep-alive");
-		requestSettings.setAdditionalHeader("Upgrade-Insecure-Requests", "1");
-		requestSettings.setAdditionalHeader(
-		"Cookie", "GUEST_LANGUAGE_ID=en_US; COOKIE_SUPPORT=true");
-		requestSettings.setAdditionalHeader(
-		"Origin", "http://1.1.1.1/login.html");
-
-		requestSettings.setRequestBody(
-		"buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&redirect_url=&network_name=Guest+Network");
-		webClient.getOptions().setUseInsecureSSL(true);
-		webClient.getPage(requestSettings);
-		webClient.close();
-	}
-
-	public static boolean testInet(String site) {
-		java.net.URL url;
-
-		try {
-			url = new java.net.URL("https://www.google.com");
-			URLConnection connection = url.openConnection();
-
-			connection.connect();
-		}
-		catch (Exception e) {
-			return false;
-		}
-
-		return true;
 	}
 
 }
